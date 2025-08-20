bb4f8577ce00f034e9d84af9b07ebd5618a6617e
cristian chinche <cristianchinchej@gmail.com>
2025-08-20 08:07:52 -0400

    mi primer commit en la nube

---
+++ Unit3.cpp
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit3.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm3 *Form3;
+//---------------------------------------------------------------------------
+__fastcall TForm3::TForm3(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+//invertir numero
+void invertir_numero(Cardinal &x){
+if(x>10){
+   byte d=x%10;
+   x=x/10;
+   invertir_numero(x);
+  byte Cant_Dig=(log10((double)x)+1);
+  x=d*pow(10,Cant_Dig)+x;
+}
+
+}
+void __fastcall TForm3::Button1Click(TObject *Sender)
+{
+   Cardinal x=Edit1->Text.ToInt();
+   invertir_numero(x);
+   Edit1->Text=x;
+}
+//---------------------------------------------------------------------------
